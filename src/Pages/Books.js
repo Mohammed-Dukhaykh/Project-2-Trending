d0c969cc53e484ffef41453d836ded0083bcdb69
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-11-19 22:47:50 +0300

    done films and book

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Card, CardGroup, Col, Row } from "react-bootstrap"
+import { Card, CardGroup, Col, Row,Image } from "react-bootstrap"
 import CardHeader from "react-bootstrap/esm/CardHeader"
 import PostsContext from "../Utils/PostsContext"
 function Books() {
@@ -9,20 +9,22 @@ function Books() {
       {books?.map(book => (
           <>
        <Card.Header as="h2" className="text-center" >{book.name}</Card.Header>
-        <Row>
+        <Row   xs={1} md={3} className="g-5 mx-4 ">
         {book.preview.map(prev=>(
-            <Col>
-            <Card>
-          <Card.Img variant="top" src={prev.imageURL} />
-          <Card.Body>
-            <Card.Title className="text-center">{prev.title}</Card.Title>
-            <Card.Text></Card.Text>
-          </Card.Body>
-          
-           
-          
-        </Card>
-        </Col>
+           <Card>
+           <Row style={{ justifyContent:"space-between"}}>
+           <Col>
+             <Image className="img-fluid rounded-start" variant="top" src={prev.imageURL} height="200px" />
+             </Col>
+             <Col>
+             
+             <Card.Body >
+               <Card.Title >{prev.title}</Card.Title>
+              
+             </Card.Body>
+             </Col>
+           </Row>
+         </Card>
         ))}
         </Row>
 </>
--- src/Pages/Movie.js
+++ src/Pages/Movie.js
@@ -1,27 +1,41 @@
 import { useContext } from "react"
 import PostsContext from "../Utils/PostsContext"
-import {Row , Col , Card, Container} from "react-bootstrap"
+import {Row , Col , Card, Container , Image} from "react-bootstrap"
 
 function Movie() {
   const { films } = useContext(PostsContext)
   return (
+
+  <body style={{backgroundColor:"black"}}>
+    
+  
+
+  <br></br>  
+    <h1 style={{textAlign:"center" , fontStyle:"italic" , color:"GhostWhite"}}>The Films are trending</h1>
       <Container>
-    <Row key={films.id} xs={1} md={4} className="g-4">
+    <Row key={films.id} xs={1} md={2} className="g-5 mx-4 ">
       {films.map(film => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w185_and_h278_bestv2" + film.poster_path} />
-            <Card.Body>
-              <Card.Title>{film.title}</Card.Title>
-              <Card.Text style={{overflow:"auto" , height:"100px"} }  >
+        <Card style={{backgroundColor:"DarkRed" } }>
+          <Row style={{ justifyContent:"space-between"}}>
+          <Col>
+            <Image className="img-fluid rounded-start" variant="top" src={"https://image.tmdb.org/t/p/w185_and_h278_bestv2" + film.poster_path} />
+            </Col>
+            <Col>
+            
+            <Card.Body >
+              <Card.Title style={{fontSize:"30px" , textAlign:"center" , color:"black" , fontWeight:"bold" , fontFamily:"serif"  }}>{film.title}</Card.Title>
+              <Card.Text style={{overflow:"hidden" , height:"100px" , textAlign:"center" , color:"white"}  } className="mt-5" >
                   {film.overview}
               </Card.Text>
             </Card.Body>
-          </Card>
-        </Col>
+            </Col>
+          </Row>
+        </Card>
       ))}
     </Row>
     </Container>
+    
+    </body>
   )
 }
 
