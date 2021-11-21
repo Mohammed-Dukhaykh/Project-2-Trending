f1ee6fa8c0c401210a93687a65748dd9200402e8
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-11-21 20:52:19 +0300

    done login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,25 +1,60 @@
 import { useContext } from "react";
-import {Container, Form , Button} from "react-bootstrap"
+import {Container, Form , Button, Card} from "react-bootstrap"
 import PostsContext from "../Utils/PostsContext";
 function Login() {
     const {login} = useContext(PostsContext)
+      
+    
     return ( 
-    <Container>
-    <Form onSubmit={login} className="mt-5">
-        <h2>Login</h2>
+
+
+    
+      
+
+      
+<>
+      
+      
+      <div class="d-flex justify-content-center" style={{ backgroundImage:`url("https://images.pexels.com/photos/212372/pexels-photo-212372.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")` , backgroundAttachment:"fixed" }} >
+        <div class="card" style={{width:"500px",height:"500px " , marginBottom:"10px" , marginTop:"20px" , paddingBottom:"1px" , backgroundImage:`url("https://images.pexels.com/photos/5274203/pexels-photo-5274203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")` }} >
+       <div >
+          
+            <h3 style={{textAlign:"center"}} >Sign In</h3>
+            <div class="d-flex justify-content-end social_icon" style={{ backgroundColor:"black "}}>
+              <span><i class="fab fa-facebook-square"></i></span>
+              <span><i class="fab fa-google-plus-square"></i></span>
+              <span><i class="fab fa-twitter-square"></i></span>
+            
+          </div>
+          <div class="card-body"   >
+            
+          <>
+     <Form onSubmit={login} className="mt-5"  >
+       
         <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="email" />
-        </Form.Group>
+        <Form.Label>Email address</Form.Label>
+         <Form.Control type="email" placeholder="Enter email" name="email" />
+      </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" name="password" />
+         <Form.Control type="password" placeholder="Password" name="password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <div className="d-flex align-items-center justify-content-center" style={{marginTop:"60px"  }}>
+       <Button  type="submit" style={{padding:"10px 30px" , backgroundImage:"none"}} >
           Login
-        </Button>
-      </Form>
-      </Container> );
+       </Button>
+       </div>
+    </Form>
+      </> 
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    </>
+    
+    
+   );
 }
 
 export default Login;
