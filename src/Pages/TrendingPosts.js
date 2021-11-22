3648a108938190ae83f9c091b54bc1f6f7fb851a
Mohammed-Dukhaykh <Mohammed.Obaidallah.abt.js@tuwaiq.edu.sa>
2021-11-22 13:10:59 +0300

    Add Share Page

--- src/Pages/TrendingPosts.js
+++ src/Pages/TrendingPosts.js
@@ -1,23 +1,36 @@
 import { useContext } from "react"
 import PostsContext from "../Utils/PostsContext"
-import { Card, Image} from "react-bootstrap"
+import { Card, Image , Row , Col, Container } from "react-bootstrap"
 
 function TrendingPosts() {
   const { trndingPost, profile } = useContext(PostsContext)
   if (!profile) return null
   // const myPosts = trndingPost.filter(item => item._user._id === profile._id )
   return (
-    <>
-      {trndingPost.map(post => (
-        <Card>
-          <Card.Header>
-           
-           <Image src={post._user.photo} height="30px" roundedCircle />
-          
+    <Container>
+    <Row xs={1} md={2} className="g-5 m-5">
+      {trndingPost.map(post => ( 
+        <Col>        
+        <Card style={{width:"20rem"}}>
+          <Card.Header className="d-flex">
+            <Image  src={post._user.photo} height="30px" roundedCircle />
+            <Card.Title as="h6">
+              {post._user.firstName} {post._user.lastName}
+            </Card.Title>
           </Card.Header>
+
+          <Card.Img src={post.image} height="320px" />
+          <Card.Footer>
+          <Card.Title as="h6">
+              {post.title}
+            </Card.Title>
+          </Card.Footer>
+        
         </Card>
+        </Col>
       ))}
-    </>
+      </Row>
+    </Container>
   )
 }
 
