5e880ee71fde41c90306092eb191a47affe5e025
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-11-23 14:21:17 +0300

    add post done

--- src/Pages/AddPosts.js
+++ src/Pages/AddPosts.js
@@ -1,30 +1,47 @@
 import { useContext } from "react"
 import {Form , Col , Row , Button} from "react-bootstrap"
 import PostsContext from "../Utils/PostsContext"
+import"../style.css"
 function AddPosts() {
     const {addPost , Profile } = useContext(PostsContext)
   return (
-    <Form onSubmit={addPost}>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Images
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="url" name="image" required />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-    <Form.Label column sm="2">
-      Description
-    </Form.Label>
-    <Col sm="10">
-    <Form.Control as="textarea" rows={3} name="description" required />
-    </Col>
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Share
-  </Button>
-    </Form>
+    <div  style={{
+     
+      backgroundImage: "linear-gradient(to right,AntiqueWhite, Bisque)",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      minHeight:"100vh" ,
+      backgroundAttachment:"fixed" ,
+      display:"flex"
+      
+    }} >
+    
+    <div class="card123">
+      <div class="card-image">	
+        <h2 class="card-heading">
+          Get started <br />
+          <small>To the world of trend</small>
+        </h2>
+      </div>
+      <form onSubmit={addPost} class="card-form">
+        <div class="input">
+          <input type="url" name="image" class="input-field" placeholder="Add a trend photo"  required/>
+          <label class="input-label"></label>
+        </div>
+              <div class="input">
+          <input type="text" name="description" class="input-field" placeholder="Add trend Title"  required/>
+          <label class="input-label"></label>
+        </div>
+             
+        <div class="action">
+          <button type="submit" class="action-button">Add</button>
+        </div>
+      </form>
+     
+    </div>
+  </div>
+  
   )
 }
 
