51c9ba254c9870339715f5497146c006324844a2
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-11-24 15:38:31 +0300

    done the cresol

--- src/Pages/TrendingPosts.js
+++ src/Pages/TrendingPosts.js
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import PostsContext from "../Utils/PostsContext"
-import { Card, Image, Row, Col, Container } from "react-bootstrap"
+import { Card, Image, Row, Col, Container,Carousel } from "react-bootstrap"
 import CardHeader from "react-bootstrap/esm/CardHeader"
 import { AiOutlineComment } from "react-icons/ai"
 import "../style.css"
@@ -11,9 +11,75 @@ function TrendingPosts() {
   return (
     <div class="trendingpost">
       <header class="trendingpagehead">
-        <h1 class="trendingtitle">People Trending</h1>
-        <p class="trendingdetails">Share Your Trending And Make The Others To See Your Posts .</p>
+      <Carousel className="d-block w-3 mx-auto  mt-6" >
+  <Carousel.Item >
+    <img style={{objectFit:"cover" , opacity:"0.4" , display:"flex"}}
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/995301/pexels-photo-995301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      alt="First slide"
+      height="500px"
+    />
+    <Carousel.Caption>
+      <h1 class="h1Carousel">Music</h1>
+      <h5>Share Your Trending And Make The Others To See Your Posts</h5>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img style={{objectFit:"cover" , opacity:"0.4" , display:"flex"}}
+      className="d-block w-100"
+      src="https://www.masdargulf.com/wp-content/uploads/2021/11/FE5pcQhXsAQD-EX.jpg"
+      alt="Second slide"
+      height="500px"
+
+      
+    />
+
+    <Carousel.Caption>
+      <h1 class="h1Carousel">Sports</h1>
+      <h5>Share Your Trending And Make The Others To See Your Posts</h5>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img style={{objectFit:"cover" , opacity:"0.4" , display:"flex"}}
+      className="d-block w-100"
+      src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2908984%2Fpexels-photo-2908984.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D650%26w%3D940"
+      alt="Third slide"
+      height="500px"
+    />
+
+    <Carousel.Caption>
+      <h1 class="h1Carousel">Books</h1>
+      <h5>Share Your Trending And Make The Others To See Your Posts</h5>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item >
+    <img style={{objectFit:"cover" , opacity:"0.4" , display:"flex"}}
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/5202925/pexels-photo-5202925.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      alt="First slide"
+      height="500px"
+    />
+    <Carousel.Caption>
+      <h1 class="h1Carousel">Movies</h1>
+      <h5>Share Your Trending And Make The Others To See Your Posts</h5>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item >
+    <img style={{objectFit:"cover" , opacity:"0.4" , display:"flex"}}
+      className="d-block w-100"
+      src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1174746%2Fpexels-photo-1174746.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D2%26h%3D650%26w%3D940"
+      alt="First slide"
+      height="500px"
+    />
+    <Carousel.Caption>
+      <h1 class="h1Carousel">Gaming</h1>
+      <h5>Share Your Trending And Make The Others To See Your Posts</h5>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
       </header>
+      
       <div class="logoImg123">
         <img  src="https://pbs.twimg.com/media/FE8GAFsX0AMX9q3?format=png&name=900x900"/>
         </div>
@@ -21,7 +87,7 @@ function TrendingPosts() {
       <div class="row">
         <div class="col-lg-11 mx-auto"></div>
       </div>
-      <div  class="row py-8">
+      <div  class="row py-5 px-5">
         {trndingPost.map(post => (
           <div class="col-lg-4">
             <figure class="posttrends1234" >
